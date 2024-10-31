54985b60429468c394a62a189b62e9832ffc7b0c
Bayrambek <bmbayrambek1600@gmail.com>
2024-10-31 21:12:59 +0500

    [add] some features and some changes not fixed

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import user_login_view, user_register_view
+from .views import user_login_view, user_register_view, reset_password_view, user_logout_view, user_edit_view
 
 urlpatterns = [
     path('login/', user_login_view, name='login'),
     path('register/', user_register_view, name='register'),
+    path('logout/', user_logout_view, name='logout'),
+    path('edit/<int:id>/', user_edit_view, name='edit'),
+    path('reset_password/', reset_password_view, name='reset-password'),
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,10 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomUserLoginForm, user_edit_form
+from django.contrib.auth import authenticate, login, logout
+
+from .models import CustomUser
+
 
 def user_register_view(request):
-    return render(request, 'Dashboard/registration/register.html')
+    print(request.POST)
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            print(form.cleaned_data)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            return redirect('login')
+    context = {
+        'form': form
+    }
+    return render(request, 'Dashboard/registration/register.html', context)
 
 def user_login_view(request):
-    return render(request, 'Dashboard/registration/login.html')
+    form = CustomUserLoginForm()
+    if request.method == 'POST':
+        form = CustomUserLoginForm(request.POST)
+        if form.is_valid():
+            new_form = form.cleaned_data
+            user = authenticate(
+                email=new_form['email'],
+                password=new_form['password']
+            )
+            if user:
+                login(request,user)
+                return redirect('dashboard-main')
+            else:
+                message = 'Invalid credentials'
+                return render(request, 'Dashboard/registration/login.html', {'form': form, 'message': message})
+    context = {
+        'form': form
+    }
+    return render(request, 'Dashboard/registration/login.html',context)
 
+def user_logout_view(request):
+    logout(request)
+    return redirect('login')
 
+def user_edit_view(request, id):
+    user = CustomUser.objects.get(id=id)
+    form = user_edit_form(instance=user)
+    message = ''
+    if request.method == 'POST':
+        form = user_edit_form(request.POST, instance=user)
+        if form.is_valid():
+            new_form = form.save(commit=False)
+            print(new_form, '*' * 10)
+            user.first_name = new_form.first_name
+            user.last_name = new_form.last_name
+            user.save()
+            return redirect('dashboard-profile')
+        else:
+            message = 'Invalid values were inputted'
+    context = {
+        'form': form,
+        'message': message
+    }
+    return render(request, 'Dashboard/profile.html')
+def reset_password_view(request):
+    return render(request, 'Openup/light/user_authentication/reset.html')
 
