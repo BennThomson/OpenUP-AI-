cbd85d0d101567081e5dbca7cd80b6a506f00699
Bayrambek <bmbayrambek1600@gmail.com>
2024-11-11 12:27:57 +0500

    [fix] profile part

--- mainPage/admin.py
+++ mainPage/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from .models import ContactModel
+from .models import ContactModel, FaqsModel
 
 @admin.register(ContactModel)
 class ContactModelAdmin(admin.ModelAdmin):
     list_filter = [
         'name', 'email'
     ]
+
+@admin.register(FaqsModel)
+class FaqsModelAdmin(admin.ModelAdmin):
+    search_field = [
+        'question'
+    ]
+    list_display = [
+        'id', 'question', 'answer'
+    ]
+
--- mainPage/models.py
+++ mainPage/models.py
@@ -12,3 +12,16 @@ class ContactModel(models.Model):
         verbose_name = 'Contact'
         verbose_name_plural = 'Contacts'
         db_table = 'contacts'
+
+class FaqsModel(models.Model):
+    question = models.CharField(max_length=100)
+    answer = models.TextField()
+
+    def __str__(self):
+        return f"Question: {self.question}"
+
+    class Meta:
+        verbose_name = 'FAQ'
+        verbose_name_plural = 'FAQs'
+        db_table = 'faqs'
+        
