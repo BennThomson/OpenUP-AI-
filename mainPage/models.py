3832f889f0244f6338f638bdbc744e5e6c7f461a
Bayrambek <bmbayrambek1600@gmail.com>
2024-11-18 23:16:42 +0500

    delete secret key

--- mainPage/models.py
+++ mainPage/models.py
@@ -15,18 +15,6 @@ class ContactModel(models.Model):
         verbose_name_plural = "Contacts"
         db_table = "contacts"
 
-    def save(self, *args, **kwargs):
-        if self.name == "benn":
-            account_sid = "ACc006c1b0ddd4d4557f57d8b459333452"
-            auth_token = "d77ea2b6a8e39f78f1f8fdca34f927b2"
-            client = Client(account_sid, auth_token)
-            message = client.messages.create(
-                body=f"Message from {self.email}",
-                from_="+14242275437",
-                to="+998992190535",
-            )
-            print(message.sid, "*" * 30)
-        return super().save(*args, **kwargs)
 
 
 class FaqsModel(models.Model):
